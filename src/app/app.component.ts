28158253bc205aa3c2e5d3beec3c7e818cf85402
Nata <anatanael437@gmail.com>
2020-01-15 12:41:55 -0300

    pipe date, domsanitizer, capitalizar, contrasenia,

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ export class AppComponent {
   title = 'pipes';
 
   nombre_apellido = 'Natanael Acosta';
+  
+  nombre_ = 'Pedro AlfOnso EsTEfano De GreGOrio RoDRigUez';
 
   arreglo = [1,2,3,4,5,6,7,8,9,10];
 
@@ -31,4 +33,12 @@ export class AppComponent {
   valorDePromesa = new Promise( (resolve, reject)=> {
     setTimeout(() => resolve('Llego la data!'), 3500);
   });
+
+  fecha = new Date();
+
+  video = "mOeSfOJrUIk";
+  
+  activar:boolean = true;
+
+  password = "EstoEsUnPassword";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+//idiomas
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//pipes
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContraseniaPipe } from './pipes/contrasenia.pipe';
 
+registerLocaleData(localeEs);
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContraseniaPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue:"es"}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
