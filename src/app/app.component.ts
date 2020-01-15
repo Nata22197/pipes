09c4130ebe2abfc816632b65197976f15d1fe394
Nata <anatanael437@gmail.com>
2020-01-14 23:57:32 -0300

    instalacion de bootstrap via npm, pipes uppercase, lowecase y slice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+
+  nombre_apellido = 'Natanael Acosta';
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+  PI = Math.PI;
 }
